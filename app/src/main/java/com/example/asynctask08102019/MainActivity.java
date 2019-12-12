707f439829620d96725377d52797ee3fcc2918af
phatdroid94 <phatdroid94@gmail.com>
2019-12-12 20:34:04 +0700

    ui async

--- app/src/main/java/com/example/asynctask08102019/MainActivity.java
+++ app/src/main/java/com/example/asynctask08102019/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.asynctask08102019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    // 1 : Param - tham số truyền vào doInBackground
+    // 2 : Progress - tham số dùng cập dữ liệu khi doIn xử lý
+    // 3 : Result - tham sau khi doIn xử lý xong trả về cho onPost
+
 }
---
+++ app/src/main/java/com/example/asynctask08102019/Toanhoc.java
@@ -0,0 +1,6 @@
+package com.example.asynctask08102019;
+
+public abstract class Toanhoc<T> {
+   protected abstract T giatriTong();
+
+}
